8e37ae8da137d2dd54c18ca7841cd1e54744651f
temoty <temoty@hotmail.com>
2017-07-12 22:39:40 -0400

    first commmits

---
+++ footer.php
@@ -0,0 +1,18 @@
+<hr>
+
+      <footer>
+        <p>&copy; <?php echo date('Y') ?> FelixTreitler.org</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+
+    
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+
+
+    <!--<?php wp_footer() ?>-->
+  </body>
+</html>
