73eb425aa893d6ab2017e5b0a42fbbb6b29ba064
temoty <temoty@hotmail.com>
2017-07-30 23:35:24 -0400

    Removed shiv and IE js scripts from header, footer, and functions.php.

--- header.php
+++ header.php
@@ -9,21 +9,11 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Jumbotron Template for Bootstrap</title>
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <title>Felix Treitler</title>
 
     <!-- Custom styles for this template -->
    
     <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
       
       <?php wp_head() ?>
       
