874efb7f69eeaa2b8609316a1f7b92fb76b7f6a5
temoty <temoty@hotmail.com>
2017-09-20 00:03:47 -0400

    eliminated nav code in header.php.

--- header.php
+++ header.php
@@ -19,35 +19,3 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-            <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>">
-                <?php bloginfo( 'name' ); ?>
-            </a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-
-            <?php 
-            
-            $args = array(
-                'menu'  => 'header-menu',
-                'menu_class' => 'nav navbar-nav',
-                'container' => 'false'
-            );
-            //wp_nav_menu( $args );
-            
-            
-            ?>
-
-        </div>
-        <!--/.navbar-collapse -->
-    </div>
-</nav>
